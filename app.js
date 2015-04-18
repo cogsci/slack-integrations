25e1e59d3d6f300053efb4611e510a5df33e5015
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2015-04-18 15:50:48 -0700

    Adding the app without routes

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
+
+// Middleware
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Loads random ASCII faces at app URL
+app.get('/', function(request, response) {
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (i=0; i < times; i++)
+      result += cool();
+  response.send(result);
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+// Router
+var router = require('./router')(app);
+
+// Error Handling
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+module.exports = app;
