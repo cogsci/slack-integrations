e1b620f1b301924d265a81373ee821300da7be67
Eric Pan <ericpan@gmail.com>
2015-04-29 03:31:11 -0700

    First try at /pomodoro

--- router/index.js
+++ router/index.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
   // For the #xdesix Slack integration
   app.use('/pew', require('./routes/pew'));
+
+  // For the /pomodoro Slack integration
+  app.use('/pomodoro', require('./routes/pomodoro'));
 };
