6a24acef984f4a1a990ce30f2e515918f9f2ddb5
Eric Pan <ericpan@gmail.com>
2015-05-04 02:08:59 -0700

    Add option for input game name after minutes

--- router/routes/pew.js
+++ router/routes/pew.js
@@ -14,6 +14,9 @@ router.post('/', function(request, response) {
   var input = request.body.text;
   var token = request.body.token;
   var message = ["<!group>: PEW"];
+  var parse = [];
+  var minutes = "";
+  var game = "";
 
   if (!token || token !== PEW_SLASH_TOKEN) {
     response.status(403).send('Unauthorized');
@@ -23,7 +26,10 @@ router.post('/', function(request, response) {
   if (!input || input.length === 0) {
     message.push("now");
   } else {
-    message.push("in", input, "minutes");
+    parse = input.split(' ');
+    minutes = parse[0];
+    game = parse[1];
+    message.push(game, "in", minutes, "minutes";
   }
 
   slack.send({
