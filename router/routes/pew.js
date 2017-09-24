0238e5f9a8d72d7f44149980bc7bda2133f5662f
Eric Pan <epan@users.noreply.github.com>
2017-09-24 16:57:12 -0700

    (refactor) Apply Prettier to pew.js

--- router/routes/pew.js
+++ router/routes/pew.js
@@ -9,17 +9,17 @@ var _ = require('lodash');
 // From slack-notify incoming webhook url
 var PEW_SLACK_WEBHOOK_URL = process.env.PEW_SLACK_WEBHOOK_URL;
 var PEW_SLASH_TOKEN = process.env.PEW_SLASH_TOKEN;
-var slack = router.slack = require('slack-notify')(PEW_SLACK_WEBHOOK_URL);
+var slack = (router.slack = require('slack-notify')(PEW_SLACK_WEBHOOK_URL));
 
 // POST /pew
 router.post('/', function(request, response) {
   var name = request.body.user_name;
   var input = request.body.text;
   var token = request.body.token;
-  var message = ["<!group>: PEW"];
+  var message = ['<!group>: PEW'];
   var parsed = [];
-  var minutes = "";
-  var game = "";
+  var minutes = '';
+  var game = '';
 
   debug('body', request.body);
 
@@ -29,12 +29,12 @@ router.post('/', function(request, response) {
   }
 
   if (!input || input.length === 0) {
-    message.push("now");
+    message.push('now');
   } else {
     parsed = input.split(' ');
     minutes = parsed[0];
     game = parsed[1];
-    message.push(game, "in", minutes, "minutes");
+    message.push(game, 'in', minutes, 'minutes');
     message = _.compact(message);
   }
 
@@ -42,7 +42,7 @@ router.post('/', function(request, response) {
 
   slack.send({
     icon_emoji: ':bomb:',
-    username: "PEW ALERT via " + name,
+    username: 'PEW ALERT via ' + name,
     text: message.join(' ')
   });
 });
